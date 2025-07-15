5b0c78f0f1b4cd937c839b3ab59d2a75144ead5e
A-mika <agniel.mickael@gmail.com>
2025-07-15 17:01:42 +0200

    changes layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} font-sans ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/src/components/ui/button";
+import { Footer } from "./components/footer";
 import { Navbar } from "./components/navigation-menu";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex min-h-screen items-center justify-center">
         <Button>Bonsoir !</Button>
       </div>
+      <Footer />
     </div>
   );
 }
